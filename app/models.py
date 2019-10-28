ad62e0476f0f87583cdc3ae07486f473114473ca
alenabozny <aleksandra.nabozny@pja.edu.pl>
2019-10-28 16:27:29 +0100

    declare db and run first migration

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import date
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +9,35 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Article(db.Model):
+    article_id = db.Column(db.Integer, primary_key=True)
+    pub_date = db.Column(db.Date)
+    access_date = db.Column(db.Date, default=date.today())
+    url = db.Column(db.String(150))
+    title = db.Column(db.String(150))
+    query = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Article "{}".'.format(self.title)
+
+
+class Sentence(db.Model):
+    sentence_id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(1000))
+    article_id = db.Column(db.Integer, db.ForeignKey('article.article_id'))
+    sequence_nr = db.Column(db.Integer) # position in the article
+    to_evaluate = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Sentence "{}" from article {}.>'.format(self.body, self.article_id)
+
+
+class Task(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.sentence_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    time_start = db.Column(db.DateTime)
+    time_end = db.Column(db.DateTime)
+    rate = db.Column(db.Enum) # CRED, NONCRED, NEUTRAL
