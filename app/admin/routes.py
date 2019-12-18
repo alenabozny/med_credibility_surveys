abab5c0d245491c883553c1dec7c258a8a2d024d
alenabozny <aleksandra.nabozny@pja.edu.pl>
2019-12-18 17:43:31 +0100

    count unassigned sentences

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -144,8 +144,19 @@ def add_tasks(user_id):
         articles = Article.query.filter(Article.title is not None).all()
 
         filtered_articles = filter(lambda article: article.sentences.__len__() > 0, articles)
-        form.article.choices = [(i.article_id, i.title + " (Sentences: " + str(i.sentences.__len__()) + ")") for i in
-                                filtered_articles]
+        # form.article.choices = [(i.article_id, i.title) for i in filtered_articles]
+
+        form.article.choices = []
+        for i in filtered_articles:
+            unassinged_tasks_num = 0
+            for s in i.sentences:
+                task = Task.query.filter_by(sentence_id=s.sentence_id).first()
+                if task:
+                    if not task.user_id:
+                        unassinged_tasks_num += 1
+
+            form.article.choices.append((i.article_id, i.title + " (Unassigned: " + str(unassinged_tasks_num)))
+
 
     if form.article.data is not None:
         tasks = Task.query.filter(Task.sentence.has(article_id=form.article.data))
